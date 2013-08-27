1cb7dc3651675ff431a3988fb6581e95e4c40463
Joe Holdcroft <joe@message.co.uk>
2013-08-27 12:04:22 +0100

    Adding Ship API service

---
+++ src/Message/Mothership/Fedex/Api/Service/Ship.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Message\Mothership\Fedex\Api\Service;
+
+/**
+ * "Ship" service.
+ *
+ * @author Joe Holdcroft <joe@message.co.uk>
+ */
+class Ship implements ServiceInterface
+{
+	/**
+	 * {@inheritDoc}
+	 */
+	public function getWsdlPath()
+	{
+		return 'cog://@Message:Mothership:Fedex::resources/wsdl/Ship/10.0.0.wsdl';
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public function getWsdlEndpoint($testMode = false)
+	{
+		return $testMode
+			? 'https://wsbeta.fedex.com:443/web-services/ship'
+			: 'https://ws.fedex.com:443/web-services/ship';
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public function getVersion()
+	{
+		return '10.0.0';
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public function getServiceName()
+	{
+		return 'ship';
+	}
+}
