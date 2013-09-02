95a61d370b949a911f2a8d71efa0eb0f94a954a5
Joe Holdcroft <joe@message.co.uk>
2013-09-02 10:57:47 +0100

    Starting to add an event listener

--- src/Message/Mothership/Fedex/EventListener.php
+++ src/Message/Mothership/Fedex/EventListener.php
@@ -2,6 +2,8 @@
 
 namespace Message\Mothership\Fedex;
 
+use Message\Mothership\Commerce\Order;
+
 use Message\Cog\Event\EventListener as BaseListener;
 use Message\Cog\Event\SubscriberInterface;
 
@@ -12,6 +14,35 @@ class EventListener extends BaseListener implements SubscriberInterface
 	 */
 	static public function getSubscribedEvents()
 	{
+		return array(
+			Order\Events::DISPATCH_POSTAGE_AUTO => array(
+				array('postageFedexExpress'),
+			),
+		);
+	}
 
+	public function postageFedexExpress(Order\Entity\Dispatch\PostageAutomaticallyEvent $event)
+	{
+		$dispatch = $event->getDispatch();
+
+		// Skip if this is not a FedEx Express dispatch
+		if ('fedex-express' !== $dispatch->method->getName()) {
+			return false;
+		}
+
+		$shipment = $this->get('fedex.api.shipment');
+		$shipment->populateFromDispatch($dispatch);
+
+		$request = new Api\Request\ProcessShipment;
+		$request->setShipment($shipment);
+
+		$response = $this->get('fedex.api.dispatcher')->dispatch($request);
+
+		// TODO: set code + cost (if defined) + Order documents on the event!
+
+		de($response);
+
+		// if it was all good, do the following:
+		$event->stopPropagation();
 	}
 }
