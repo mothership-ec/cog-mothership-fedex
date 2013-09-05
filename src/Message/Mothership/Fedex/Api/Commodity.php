cbf1a2eb1e98049e9d867f45c9ca805694cbbcec
Joe Holdcroft <joe@message.co.uk>
2013-09-05 18:39:39 +0100

    Adding FedEx commodity

--- src/Message/Mothership/Fedex/Api/Commodity.php
+++ src/Message/Mothership/Fedex/Api/Commodity.php
@@ -2,12 +2,48 @@
 
 namespace Message\Mothership\Fedex\Api;
 
-use Message\Mothership\Commerce\Order\Item\Item;
+use Message\Mothership\Commerce\Order\Entity\Item;
 
+/**
+ * Represents a FedEx "Commodity" (essentially a product in a package/shipment).
+ *
+ * @author Joe Holdcroft <joe@message.co.uk>
+ */
 class Commodity
 {
-	public function populateFromItem(Item $item)
-	{
+	public $quantity;
+	public $description;
+	public $manufactureCountryID;
+	public $weight; // as always, in grams
 
+	public $currencyID;
+	public $price;
+	public $customsValue;
+	public $insuredValue;
+
+	/**
+	 * Populate this Commodity from an order item row.
+	 *
+	 * @todo Ensure the Product from `Item::getProduct()` returns the Product in
+	 *       the correct locale for the order, so the currency is correct.
+	 *
+	 * @param  Item\Row $row The item row to populate this Commodity from
+	 *
+	 * @return Commodity     Returns $this for chainability
+	 */
+	public function populateFromItemRow(Item\Row $row)
+	{
+		$product = $row->first()->getProduct();
+
+		$this->quantity             = $row->count();
+		$this->description          = $product->exportDescription;
+		$this->manufactureCountryID = $product ? $product->exportManufactureCountryID : null;
+		$this->weight               = $row->sum('weight');
+		$this->currencyID           = $row->first()->order->currencyID;
+		$this->customsValue         = $row->sum('net') + $row->sum('discount'); // Ex-Tax Price (including discounts)
+		$this->price                = $this->customsValue / $this->quantity;
+		$this->insuredValue         = $product ? $product->exportValue : 0;
+
+		return $this;
 	}
 }
